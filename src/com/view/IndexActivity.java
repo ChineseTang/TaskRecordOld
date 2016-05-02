d344a4821ad8a5e9005fabe1c0a1ba8e073ea4fa
ChineseTang <tzjsmile@qq.com>
2016-05-02 22:00:58 +0800

    添加了日期的功能，有日历了

--- src/com/view/IndexActivity.java
+++ src/com/view/IndexActivity.java
@@ -1,6 +1,10 @@
 package com.view;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.myview.MonthDateView;
+import com.myview.MonthDateView.DateClick;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
@@ -13,10 +17,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class IndexActivity extends BaseActivity implements OnItemClickListener {
 	private DrawerLayout mDrawerLayout;
@@ -25,12 +33,20 @@ public class IndexActivity extends BaseActivity implements OnItemClickListener {
 	private ArrayAdapter<String> adapter;
 	private ActionBarDrawerToggle mDrawerToggle;
 	private String mtitle;
+	private ImageView iv_left;
+	private ImageView iv_right;
+	private TextView tv_date;
+	private TextView tv_week;
+	private TextView tv_today;
+	private MonthDateView monthDateView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_index);
+		getActionBar().setDisplayShowHomeEnabled(false); //取消actionbar左侧的快捷图标和文字
+		//getActionBar().setDisplayShowTitleEnabled(false); 
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+		mDrawerList = (ListView) findViewById(R.id.left_drawers);
 		menuLists = new ArrayList<String>();
 		mtitle = (String) getTitle();
 		menuLists.add("首页");
@@ -59,7 +75,7 @@ public class IndexActivity extends BaseActivity implements OnItemClickListener {
 					@Override
 					public void onDrawerOpened(View drawerView) {
 						super.onDrawerOpened(drawerView);
-						getActionBar().setTitle("请选择");
+						getActionBar().setTitle("事务记录");
 						invalidateOptionsMenu();
 					}
 		};
@@ -68,8 +84,55 @@ public class IndexActivity extends BaseActivity implements OnItemClickListener {
 		//begin the app icon 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setHomeButtonEnabled(true);
+		
+		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(10);
+		list.add(12);
+		list.add(15);
+		list.add(16);
+		iv_left = (ImageView) findViewById(R.id.iv_left);
+		iv_right = (ImageView) findViewById(R.id.iv_right);
+		monthDateView = (MonthDateView) findViewById(R.id.monthDateView);
+		tv_date = (TextView) findViewById(R.id.date_text);
+		tv_week  =(TextView) findViewById(R.id.week_text);
+		tv_today = (TextView) findViewById(R.id.tv_today);
+		monthDateView.setTextView(tv_date,tv_week);
+		monthDateView.setDaysHasThingList(list);
+		monthDateView.setDateClick(new DateClick() {
+			
+			@Override
+			public void onClickOnDate() {
+				Toast.makeText(getApplication(), "点击了：" + monthDateView.getmSelDay(), Toast.LENGTH_SHORT).show();
+			}
+		});
+		setOnlistener();
 	}
-
+	private void setOnlistener(){
+		iv_left.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				monthDateView.onLeftClick();
+			}
+		});
+		
+		iv_right.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				monthDateView.onRightClick();
+			}
+		});
+		
+		tv_today.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				monthDateView.setTodayToView();
+			}
+		});
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -100,6 +163,8 @@ public class IndexActivity extends BaseActivity implements OnItemClickListener {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	//当点击不同的选项时对应不同的Fragement
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
 		// insert into a fragment into the FrameLayout
